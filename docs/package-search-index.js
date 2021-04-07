44eb45dee8873c09832701d1e9b05da1bc088821
Hugo De Moraes <demoraeshugo@gmail.com>
2021-04-07 14:27:24 -0400

    renamed /javadoc to /docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sample"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"view"}];updateSearchResults();
